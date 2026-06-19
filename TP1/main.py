84946a912d8c58774c8a2a99661dc44e52a46a69
justinedastous <justine.dastous@gmail.com>
2026-06-19 14:07:16 -0400

    Changement de l'écriture de la trace

--- TP1/main.py
+++ TP1/main.py
@@ -1,5 +1,11 @@
-# Devoir 1 - IFT615 : Intelligence artificielle
-# Membres de l'équipe : Charles Ally, Justine d'Astous
+"""
+Devoir 1 - IFT615 : Intelligence artificielle
+Membres de l'équipe :
+    Charles Ally (ALLC7158),
+    Alexy Beaulieu (BEAA6878),
+    Justine d'Astous (DASJ6993),
+    Alex Phan (PHAA1597),
+"""
 
 from os import write
 import numpy as np
