5a9b67824d9d50e48c603aa102cad6ddbad17a00
justinedastous <justine.dastous@gmail.com>
2026-07-15 20:14:00 -0400

    ajout de nos noms

--- TP2/main.py
+++ TP2/main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+"""
+Devoir 2 - IFT615 : Intelligence artificielle
+Membres de l'équipe :
+    Charles Ally (ALLC7158),
+    Alexy Beaulieu (BEAA6878),
+    Justine d'Astous (DASJ6993),
+    Alex Phan (PHAA1597),
+"""
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
